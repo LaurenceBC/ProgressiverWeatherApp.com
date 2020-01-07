cb41eb665169dd8841b8ca33e8a84ffa1f2938cf
larry <larrybc@gmail.com>
2020-01-07 17:09:33 +0000

    Added silviolleite/laravel-pwa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
+    @laravelPWA
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <!-- Fonts -->
